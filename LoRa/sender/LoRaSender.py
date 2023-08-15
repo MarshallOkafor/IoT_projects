a4bbf41561d3900d8946fa8f04ccd36a435d2be3
Marshall Okafor <marshallokafor@Marshalls-MBP.attlocal.net>
2023-08-14 20:14:14 -0500

    fixed bugs and added bme680 library file

--- LoRa/sender/LoRaSender.py
+++ LoRa/sender/LoRaSender.py
@@ -8,7 +8,7 @@ Description: Simulated weather station using LoRa, ESP32 device, MQTT broker and
 
 import time
 import ssd1306
-import BME280
+from bme680 import *
 import config_lora
 from machine import Pin, I2C
 from time import sleep
@@ -18,14 +18,11 @@ import json
 # Device ID
 device_id = config_lora.get_nodename()
 
-"""
-These lines of code are disabled since there is no physical sensor connected to ESP32
+# ESP32 Sensor - Pin assignment for Heltec LoRa V2
+i2c = I2C(scl=Pin(15), sda=Pin(4), freq=10000)
+bme = BME280_I2C(i2c=i2c)
 
-# ESP32 Sensor - Pin assignment
-i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
-bme = BME280.BME280(i2c=i2c)
 """
-
 # Heltec LoRa 32 with OLED Display
 oled_width = 128
 oled_height = 64
@@ -48,56 +45,24 @@ i2c = I2C(scl=i2c_scl, sda=i2c_sda)
 # Create the display object
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 oled.fill(0)
-
-
-def temperature():
-  
-  # Randomly generated temperature readings
-  
-  t = random.uniform(20.00, 50.00)
-  temp = '{:.2f}'.format(t)
-  return temp
-
-def humidity():
-  """
-  Randomly generated humidity readings
-  """
-  h = random.uniform(30.00, 70.00)
-  hum = float("{:.2f}".format(h))
-  return hum
-
-def pressure():
-  """
-  Randomly generated pressure readings
-  """
-  p = random.uniform(500.00, 1000.00)
-  pres = "{:.2f}".format(p)
-  return pres
+"""
 
 def read_bme_sensor():
   try:
-    """
-    These lines of code are disabled since there is no physical sensor connected to ESP32
-    temp = b'%s' % bme.temperature[:-1]
-    hum = b'%s' % bme.humidity[:-1]
-    pres = b'%s'% bme.pressure[:-3]
-  
-    """
-    # Hard-coded sensor readings as test readings
-    temp = b'%s' % temperature()
-    hum = b'%s' % humidity()
-    pres = b'%s' % pressure()
-   
+    temp = b'%s' % str(round(bme.temperature, 2)) + ' C'
+    hum = b'%s' % str(round(bme.humidity, 2)) + ' %'
+    pres = b'%s'% str(round(bme.pressure, 2)) + ' hPa'
+    gas = b'%s'% str(round(bme.gas/1000, 2)) + ' KOhms'
 
-    return float(temp), float(hum), float(pres)
-    
+    return temp, hum, pres, gas  
   except OSError as e:
     return('Failed to read sensor.')
 
 def getCurrentTime(someTime):
     """
-    Get current date and time
+    Format current date and time
     """
+
     date = ''
     time = ''
 
@@ -115,37 +80,42 @@ def getCurrentTime(someTime):
 
     return date + ' ' + time
 
+# 946684800 is the UNIX timestamp for Jan 1, 2000
+adjusted_unix_time = 1692059206 - 946684800
+
 # Code to update the receiver
 def send(lora):
     
     print('LoRa Temperature Sender\n')
 
     while True:
-        date_time = getCurrentTime(time.localtime())
+        date_time = getCurrentTime(time.localtime(adjusted_unix_time))
         print('Measuring weather conditions... ', end="")
         print('Reporting to LoRa Receiver.\n')
 
-        temp, hum, pres = read_bme_sensor()
+        temp, hum, pres, gas = read_bme_sensor()
 
         payload = json.dumps({
             'dateTime': date_time,
             'deviceId': device_id, 
             'temp': temp,   
             'hum': hum,      
-            'pres': pres
+            'pres': pres,
+            'gas': gas
             })
 
         print("Sending packet: \n")
         print(payload)
+        """
         oled.text('Sending Data...', 0, 0)
         oled.text(date_time, 0, 10)
         oled.text('Temp: ' + str(temp) + 'C', 0, 20)
         oled.text('Hum: ' + str(hum) + '%', 0, 30)
         oled.text('Pres: ' + str(pres) + 'hPa', 0, 40)
         oled.show()
+        """
 
         lora.println(payload)
 
-        
-        oled.fill(0)
+        #oled.fill(0)
         sleep(5)
